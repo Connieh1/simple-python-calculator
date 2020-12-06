c8a21ccabc2df6dee176c645340a25bc10c3d148
github username <constance.hyman@mchsi.com>
2020-12-06 09:26:01 -0800

    Addition condition

--- simple-python-calculator.py
+++ simple-python-calculator.py
@@ -14,3 +14,16 @@ def multiply(a,b):
 def divide(a,b):
     return(a/b)
 
+print("Select Operation: ")
+print("1: add")
+print("2: subtract")
+print("3: multiply")
+print("4: divide")
+
+choice=input("Enter choice 1/2/3/4: ")
+num_1=int(input("Enter first number: "))
+num_2=int(input("Enter second number: "))
+
+if choice=="1":
+    print(num_1, "+", num_2, "=", add(num_1, num_2))
+
