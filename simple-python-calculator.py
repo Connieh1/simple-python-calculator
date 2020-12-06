066f093c4ad1931925bdb07e8db7f4677de5780e
github username <constance.hyman@mchsi.com>
2020-12-06 09:31:59 -0800

    Division condition

--- simple-python-calculator.py
+++ simple-python-calculator.py
@@ -30,3 +30,5 @@ elif choice=="2":
     print(num_1, "-", num_2, "=", subtract(num_1, num_2))
 elif choice=="3":
     print(num_1, "*", num_2, "=", multiply(num_1, num_2))
+elif choice=="4":
+    print(num_1, "/", num_2, "=", divide(num_1, num_2))
